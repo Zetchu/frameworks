79073f55d7d8bbf98c7381cb146b96247121a998
Zetchu <shmungula@gmail.com>
2026-03-18 23:39:48 +0100

    feat: add counter functionality with increment, decrement, and reset buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
 import heroImg from './assets/hero.png';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((count) => count + 1);
+  }
+  function decrement() {
+    setCount((count) => count - 1);
+  }
+  function reset() {
+    setCount(0);
+  }
 
   return (
     <>
@@ -13,17 +22,13 @@ function App() {
         <div className='hero'>
           <img src={heroImg} className='base' width='170' height='179' alt='' />
           <img src={reactLogo} className='framework' alt='React logo' />
-          <img src={viteLogo} className='vite' alt='Vite logo' />
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+        <div className='counter'>Count is {count}</div>
+        <div className='card'>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          <button onClick={reset}>Reset</button>
         </div>
-        <button className='counter' onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
       </section>
 
       <div className='ticks'></div>
